2ef8074b30348047ca3e7c55b62f150bd3693a63
Katerinka58 <144324135+Katerinka58@users.noreply.github.com>
2024-05-08 10:51:23 +0300

    Update Programm.cs

--- Programm.cs
+++ Programm.cs
@@ -1,162 +1,56 @@
 using System;
-using System.Collections.Generic;
 
-public abstract class Transport
+namespace ConsoleApp
 {
-    public abstract double Speed { get; }
-    public abstract double Capacity { get; }
-    public abstract double Range { get; }
-
-    public abstract double GetSpeed();
-    public abstract double GetCapacity();
-    public abstract double GetRange();
-}
-
-public abstract class Car : Transport
-{
-    public override double Speed { get; }
-    public override double Capacity { get; }
-    public override double Range { get; }
-
-    public override double GetSpeed() => Speed;
-    public override double GetCapacity() => Capacity;
-    public override double GetRange() => Range;
-}
-
-public abstract class Airplane : Transport
-{
-    public override double Speed { get; }
-    public override double Capacity { get; }
-    public override double Range { get; }
-
-    public override double GetSpeed() => Speed;
-    public override double GetCapacity() => Capacity;
-    public override double GetRange() => Range;
-}
-
-// Абстрактный класс "Корабль"
-public abstract class Ship : Transport
-{
-    public override double Speed { get; }
-    public override double Capacity { get; }
-    public override double Range { get; }
-
-    public override double GetSpeed() => Speed;
-    public override double GetCapacity() => Capacity;
-    public override double GetRange() => Range;
-}
-
-// Конкретный класс "Легковой автомобиль"
-public class PassengerCar : Car
-{
-    public override double Speed { get; } = 120.0;
-    public override double Capacity { get; } = 500.0;
-    public override double Range { get; } = 500.0;
-
-    public override string ToString()
+    internal class Program
     {
-        return $"Легковой автомобиль грузоподъемностью {GetCapacity()} может проехать {GetRange()}";
-    }
-}
-
-public class Airplane1 : Airplane
-{
-    public override double Speed { get; } = 800.0;
-    public override double Capacity { get; } = 10000.0;
-    public override double Range { get; } = 5000.0;
-
-    public override string ToString()
-    {
-        return $"Самолет грузоподъемностью {GetCapacity()} может пролететь {GetRange()}";
-    }
-}
-
-public class Ship1 : Ship
-{
-    public override double Speed { get; } = 20.0;
-    public override double Capacity { get; } = 10000.0;
-    public override double Range { get; } = 10000.0;
-
-    public override string ToString()
-    {
-        return $"Корабль грузоподъемностью {GetCapacity()} может проплыть {GetRange()}";
-    }
-}
-
-public class User
-{
-    private string _login;
-    private string _password;
-
-    public User(string login, string password)
-    {
-        _login = login;
-        _password = password;
-    }
-
-    public string Login
-    {
-        get { return _login; }
-    }
-
-    public string Password
-    {
-        get { return _password; }
-    }
-}
-
-// Класс "Менеджер пользователей"
-public class UserManager
-{
-    private List<User> _users;
-
-    public UserManager()
-    {
-        _users = new List<User>();
-    }
-
-    public void RegisterUser(string login, string password)
-    {
-        User user = new User(login, password);
-        _users.Add(user);
-    }
-
-    public bool Login(string login, string password)
-    {
-        foreach (var user in _users)
+        static void Main(string[] args)
         {
-            if (user.Login == login && user.Password == password)
-            {
-                return true;
-            }
-        }
-        return false;
-    }
-}
+            Car car = new Car("300 км", "300", "120");
+            Console.WriteLine(car.GetInfo(car));
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        UserManager userManager = new UserManager();
+            Ship ship = new Ship("чуть более пяти лет без дозаправки","10000", "30");
+            Console.WriteLine(ship.GetInfo(ship));
 
-        userManager.RegisterUser("user1", "password1");
-        userManager.RegisterUser("user2", "password2");
-        userManager.RegisterUser("user3", "password3");
-
-        Console.WriteLine("Вход в систему:");
-        Console.Write("Введите логин: ");
-        string login = Console.ReadLine();
-        Console.Write("Введите пароль: ");
-        string password = Console.ReadLine();
-
-        if (userManager.Login(login, password))
-        {
-            Console.WriteLine("Вход выполнен успешно!");
-        }
-        else
-        {
-            Console.WriteLine("Неверный логин или пароль.");
+            Airplane airplane = new Airplane("16000 км", "23000", "850");
+            Console.WriteLine(airplane.GetInfo(airplane));
         }
     }
+    public abstract class Transport
+    {
+        public string Speed { get; set; }
+        public string Carrying { get; set; }
+        public string Path { get; set; }
+        public string GetInfo(Transport transport)
+        {
+            string transportType = transport.GetType().Name;
+            if (transportType == "Car") transportType = "Машина";
+            if (transportType == "Ship") transportType = "Корабль";
+            if (transportType == "Airplane") transportType = "Самолет";
+            return $"{transportType} грузоподъемностью в {Carrying} кг может преодолеть {Path} со скоростью {Speed}";
+        }
+        public Transport(string path, string carrying, string speed) 
+        {
+            Speed = speed;
+            Carrying = carrying;
+            Path = path;
+        }
+        public abstract string ForOverride { get; protected set; }
+    }
+    public class Car : Transport
+    {
+        public Car(string speed, string carrying, string path) : base(speed, carrying, path) { }
+        public override string ForOverride { get; protected set; }
+    }
+    public class Ship : Transport
+    {
+        public Ship(string speed, string carrying, string path) : base(speed, carrying, path) { }
+        public override string ForOverride { get; protected set; }
+    }
+    public class Airplane : Transport
+    {
+        public Airplane(string speed, string carrying, string path) : base(speed, carrying, path) { }
+        public override string ForOverride { get; protected set; }
+    }
+
 }
